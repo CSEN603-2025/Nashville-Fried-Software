fb2bf61fa02f46c2ef70d6afa84696cb6727356d
sultann-301 <91574726+sultann-301@users.noreply.github.com>
2025-05-08 14:16:52 +0300

    fixed khaled imports

--- FRONT_END/my-app/src/App.js
+++ FRONT_END/my-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import './Appontments.tsx';
+import './Appointments.tsx';
 import AppointmentsPage from './Appointments.tsx';
 import ContactBook from './Components/Appointments/ContactBook.tsx';
 
