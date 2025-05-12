6bf3aed7c7d747ffd4b5fa42cedfba7ab1a3afab
ahmedbr1 <ahmed.sherif0120055942@gmail.com>
2025-05-12 16:40:32 +0300

    A minor error in the conditional rendering of links in the scad/faculty sidebar which had caused the faculty links to have no classname and thus no styling has been patched

--- FRONT_END/my-app/src/Components/Dashboard/SideBarSCAD.tsx
+++ FRONT_END/my-app/src/Components/Dashboard/SideBarSCAD.tsx
@@ -11,11 +11,11 @@ const SideBar: React.FC<SCADStatsProps> = ({ scad, active }) =>
     return (
             <div className="link-container">
                 <h2>Welcome, SCAD</h2>
-                {scad ? <Link className={active == 'Dashboard' ? 'active-link' : 'link'} to="/scaddashboard">Dashboard</Link> : <Link to="/facultydashboard">Dashboard</Link>}
+                {scad ? <Link className={active == 'Dashboard' ? 'active-link' : 'link'} to="/scaddashboard">Dashboard</Link> : <Link className={active == 'Dashboard' ? 'active-link' : 'link'} to="/facultydashboard">Dashboard</Link>}
                 {scad && (<Link className={active == 'Students' ? 'active-link' : 'link'} to="/Students">Students</Link>)}
                 {scad && (<Link className={active == 'Companies' ? 'active-link' : 'link'} to="/CompanySCAD">Companies</Link>)}
                 {scad && (<Link className={active == 'Workshops' ? 'active-link' : 'link'} to="/WorkshopSCAD">Workshops</Link>)}
-                {scad ? <Link className={active == 'Reports' ? 'active-link' : 'link'} to="/ReportSCAD">Reports</Link> : <Link to="/ReportFaculty">Reports</Link>}
+                {scad ? <Link className={active == 'Reports' ? 'active-link' : 'link'} to="/ReportSCAD">Reports</Link> : <Link className={active == 'Reports' ? 'active-link' : 'link'} to="/ReportFaculty">Reports</Link>}
                 {scad && (<Link className={active == 'Internships' ? 'active-link' : 'link'} to="/Internshipscad">Available Internships</Link>)}
 
                 <span className="scadtopia dashboard-span">SCADtopia</span>
